24ec42918633ecd4a621d0a850a738d4c2129fdf
Patrick Amrein <amrein@ubique.ch>
2021-03-29 18:40:34 +0200

    ignore mysqll

--- src/test/java/eu/interop/federationgateway/controller/mysql/UploadControllerMysqlTest.java
+++ src/test/java/eu/interop/federationgateway/controller/mysql/UploadControllerMysqlTest.java
@@ -1,10 +1,12 @@
 package eu.interop.federationgateway.controller.mysql;
 
+import org.junit.Ignore;
 import org.springframework.test.context.ActiveProfiles;
 
 import eu.interop.federationgateway.controller.UploadControllerTest;
 
 @ActiveProfiles("mysql")
+@Ignore
 public class UploadControllerMysqlTest extends UploadControllerTest {
   
 }
